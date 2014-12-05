39b91bd763e97188ec7006b779aca197983fdc6c
IsamuSosamu <ya.isamu@gmail.com>
2014-12-05 11:27:32 +0600

    321

---
+++ src/LAB 6/LAB 6/Service1.svc.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace LAB_6
+{
+     public class Service1 : IService1
+    {
+
+        public string Calculate(string number,string first,string action)
+        {
+            if (action.Equals("add"))
+            {
+                first = (double.Parse(first) + double.Parse(number)).ToString();
+                return first;
+            }
+            else if (action.Equals("mult"))
+            {
+                first = (double.Parse(first) * double.Parse(number)).ToString();
+                return first;
+         
+            }
+            else if (action.Equals("div"))
+            {
+                first = (double.Parse(first) / double.Parse(number)).ToString();
+                return first;
+         
+            }else if (action.Equals("sub"))
+            {
+                first = (double.Parse(first) - double.Parse(number)).ToString();
+                return first;
+         
+            }
+            return number;
+        }
+       
+
+         public string Sin(string number)
+        {
+            return Math.Sin(double.Parse(number)).ToString();
+        }
+
+         public string Cos(string number)
+        {
+            return Math.Cos(double.Parse(number)).ToString();
+        }
+
+       public string Exp(string number)
+        {
+            return Math.Exp(double.Parse(number)).ToString();
+        }
+    }
+}
+
+
+
